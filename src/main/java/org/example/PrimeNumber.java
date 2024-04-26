249d05de2e2b82eddc4929d460e002327da828a3
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2024-04-26 19:09:56 +0530

    Prime Number

---
+++ src/main/java/org/example/PrimeNumber.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        int a = 57;
+        System.out.println(primeornot(a));
+
+    }
+
+    public static String primeornot(int num) {
+        boolean bool = false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0)
+                bool = true;
+        }
+
+        if (bool == true) {
+            return "Number is not prime";
+        } else {
+            return "Number is prime";
+        }
+    }
+
+}
